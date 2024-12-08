c9bb50874aac4a86e8126a3e70f928df6734b650
narmis99 <narmis99@gmail.com>
2024-12-08 17:12:53 -0500

    configured daisy ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+
+<!-- src/routes/+page.svelte -->
+<script>
+	// Any necessary script logic can go here
+</script>
+
+<div class="p-4">
+	<!-- DaisyUI Button -->
+	<button class="btn btn-primary">Test Button</button>
+</div>
