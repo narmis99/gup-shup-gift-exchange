2277f91d871ec0c23fddc1add85feaa61fd2ba35
narmis99 <narmis99@gmail.com>
2025-01-20 16:58:37 -0500

    chore: login cleanup

--- src/lib/components/LoginModal.svelte
+++ src/lib/components/LoginModal.svelte
@@ -1,56 +1,14 @@
 <!-- A <script> tag with a module attribute runs once when the module first evaluates, rather than for each component instance. Variables declared in this block can be referenced elsewhere in the component, but not vice versa. -->
 <script lang="ts">
 	// @ts-nocheck
-	// export let form;
 	import { createEventDispatcher } from 'svelte';
 	import { enhance, applyAction } from '$app/forms';
-
-	// let formData = $state({ username: '', passkey: '', error: '' });
 
 	const dispatch = createEventDispatcher();
 
 	let { openModal = $bindable(), form } = $props();
-	$inspect(form);
-
-	if (form?.data.success) {
-		openModal = false;
-		dispatch(form.data.username);
-	}
-
-	// function closeLoginModal() {
-	// 	openModal = false;
-	// 	dispatch('close');
-	// }
-
-	/*	async function handleSubmit(event) {
-		console.log('handleSubmit in LoginModal.svelte js');
-		// console.log('formData: ' + JSON.stringify(formData));
-		event.preventDefault();
-		// formData.error = '';
-
-		try {
-			const response = await fetch('/api/auth.js', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({ username, passkey })
-			});
-
-			console.log('resposnse: ' + JSON.stringify(response));
-			const result = await response.json();
-			console.log('result: ' + JSON.stringify(result));
-
-			// if (response.ok) {
-			// 	dispatch('closeModal');
-			// } else {
-			// 	formData.error = result.message || 'Invalid credentials';
-			// }
-		} catch (err) {
-			formData.error = 'Error connecting to the server - ' + err;
-		}
-	}*/
 </script>
 
-<!-- <button class="btn btn-primary" onclick={openLoginModal}>Log in</button> -->
 
 <div class="modal" class:modal-open={openModal}>
 	<div class="modal-box">
@@ -87,8 +45,11 @@
 
 					return async ({ result, update }) => {
 						form = result;
-						console.log('form: ' + JSON.stringify(form.data));
-						await applyAction(result);
+						console.log('result: ' + JSON.stringify(result.type));
+						if (result.type == 'success') {
+							openModal = false;
+							dispatch(form.data.username);
+						}
 						// `result` is an `ActionResult` object
 						// `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
 					};
@@ -113,29 +74,7 @@
 					/>
 				</label>
 				<button type="submit" class="btn">Submit</button>
-				<!-- <button onclick={handleSubmit} class="btn">Login</button> -->
 			</form>
 		</div>
 	</div>
 </div>
-
-<!-- {#if formData.error}
-					<div class="toast toast-center toast-top">
-						<div class="alert alert-warning">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="h-6 w-6 shrink-0 stroke-current"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-								/>
-							</svg>
-							<span>STODO: message here</span>
-						</div>
-					</div>
-				{/if} -->
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-import { json } from '@sveltejs/kit';
 import { fail } from '@sveltejs/kit';
 import type { Actions } from '../../.svelte-kit/types/src/routes/$types';
 import bcrypt from 'bcryptjs';
@@ -7,14 +5,10 @@ import { pool } from '../lib/server/database';
 
 export const actions: Actions = {
 	default: async ({ request }) => {
-		console.log('POST request: ' + JSON.stringify(request));
-
 		// parse form data
 		const loginData = await request.formData();
 		const username = loginData.get('username');
-		console.log('username: ' + username);
 		const passkey = loginData.get('passkey');
-		console.log('passkey: ' + passkey);
 
 		try {
 			// validate user in the database
@@ -22,49 +16,17 @@ export const actions: Actions = {
 			const result = await pool.query(query, [username]);
 
 			if (result.rows.length === 0) {
-				console.log('fail 1');
 				return fail(400, { error: 'Invalid credentials', username });
-				// return fail({ username, error: 'Invalid credentials' });
 			}
 			const isValid = await bcrypt.compare(passkey, result.rows[0].passkey);
 
 			if (!isValid) {
-				console.log('fail 2');
 				return fail(400, { error: 'Invalid credentials', username });
-				// return fail({ username, error: 'Invalid credentials' });
 			}
 
 			return { success: true };
 		} catch (error) {
-			console.log('database error: ' + error);
-			return fail(500, { error: 'Internal server error' });
+			return fail(500, { error: 'Internal server error: ' + error });
 		}
 	}
 };
-
-
-
-		// try {
-		// 	// Query the database for the user
-		// 	const { rows } = await pool.query('SELECT passkey FROM users WHERE username = $1', [
-		// 		username
-		// 	]);
-
-		// 	if (rows.length === 0) {
-		// 		return { error: 'User not found' };
-		// 	}
-
-		// 	const hashedPasskey = rows[0].passkey;
-
-		// 	// Validate the passkey
-		// 	const isValid = bcrypt.compareSync(passkey, hashedPasskey);
-		// 	if (!isValid) {
-		// 		return { error: 'Invalid credentials' };
-		// 	}
-
-		// 	// On successful login, you can redirect or set session data
-		// 	return { success: true };
-		// } catch (error) {
-		// 	console.error(error);
-		// 	return { error: 'An internal error occurred. Please try again later.' };
-		// }
