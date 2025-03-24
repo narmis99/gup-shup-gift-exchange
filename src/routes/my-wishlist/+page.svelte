129d7b39d3c2acb7cc46b12ed8e3dbbde9f344e7
narmis99 <narmis99@gmail.com>
2025-03-24 00:03:31 -0400

    feat: openai dependency installed and tested

--- src/routes/my-wishlist/+page.svelte
+++ src/routes/my-wishlist/+page.svelte
@@ -1,22 +1,32 @@
 <!-- The wishes created by the user are loaded upon page navigation and then passed to children components to handle display. -->
 <script lang="ts">
 	import WishModal from '$lib/components/WishModal.svelte';
+	import SantaBantaModal from '$lib/components/SantaBantaModal.svelte';
 	import WishCard from '$lib/components/WishCard.svelte';
 
 	let { data, form } = $props();
 	let showWishModal: boolean = $state(false);
+	let showSantaBantaModal: boolean = $state(false);
 </script>
 
 <div class="m-4 w-full">
 	<div>
 		<div class="flex justify-between">
 			<h1 class="text-xl font-bold">My Wishlist</h1>
-			<button
-				class="btn btn-accent"
-				onclick={() => {
-					showWishModal = true;
-				}}>+ Add Wish</button
-			>
+			<div>
+				<button
+					class="btn btn-secondary"
+					onclick={() => {
+						showSantaBantaModal = true;
+					}}>Ask Santa Banta ✨</button
+				>
+				<button
+					class="btn btn-accent"
+					onclick={() => {
+						showWishModal = true;
+					}}>+ Add Wish</button
+				>
+			</div>
 		</div>
 
 		<div class="w-full columns-sm space-y-5 p-5">
@@ -29,4 +39,8 @@
 
 {#if showWishModal}
 	<WishModal bind:openModal={showWishModal} />
+{/if}
+
+{#if showSantaBantaModal}
+	<SantaBantaModal bind:openModal={showSantaBantaModal} />
 {/if}
